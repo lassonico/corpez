d44d4d61c15a38ae3e457cdf4e96b4fe329d8454
lassonico <sionvmx@gmail.com>
2023-09-01 17:01:21 -0500

    cambio de sintaxis

--- appajx.js
+++ appajx.js
@@ -1,109 +1,156 @@
+const formulario = document.querySelector('#transactionForm');
 document.addEventListener('DOMContentLoaded', function () {
-    const formulario = document.querySelector('#transactionForm');
-    formulario.addEventListener('submit', (e) => {
-      e.preventDefault();
-      enviarFormulario();
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    enviarFormulario();
     });
-  });
-  
-  function enviarFormulario() {
+});
+
+function enviarFormulario() {
+
     const monto = parseInt(document.getElementById('monto').value);
     const tipoDoc = document.getElementById('tipoDoc').value;
     const documento = document.getElementById('documento').value;
     const nombre = document.getElementById('nombre').value;
-  
+
     // Datos de autenticación
     const usuario = 'LUIS1234';
     const contrasena = 'BDtn72@5';
-  
+
     // URL del primer servicio
     const primerServicioURL = 'https://s5s6nqk77i.execute-api.us-east-1.amazonaws.com/get_tokens_convenio_empresa';
-  
+
     // Datos para el cuerpo de la primera solicitud
     const datosPrimeraSolicitud = {
-      username: usuario,
-      password: contrasena
+        username: usuario,
+        password: contrasena
     };
-  
+
     // Convertir los datos del cuerpo a formato JSON
     const datosPrimeraSolicitudJSON = JSON.stringify(datosPrimeraSolicitud);
-  
+
     // Crear una nueva instancia de XMLHttpRequest para la primera solicitud
     const xhrPrimeraSolicitud = new XMLHttpRequest();
-  
+
     // Configurar la primera solicitud POST
     xhrPrimeraSolicitud.open('POST', primerServicioURL, true);
-  
+
     // Configurar el encabezado de la primera solicitud
     xhrPrimeraSolicitud.setRequestHeader('Content-Type', 'application/json');
-  
+
     // Manejar la respuesta cuando se complete la primera solicitud
     xhrPrimeraSolicitud.onreadystatechange = function () {
-      if (xhrPrimeraSolicitud.readyState === 4) {
+        if (xhrPrimeraSolicitud.readyState === 4) {
         if (xhrPrimeraSolicitud.status === 200) {
-          // La primera solicitud se completó con éxito
-          const response = JSON.parse(xhrPrimeraSolicitud.responseText);
-          const idToken = response.data.IdToken;
-  
-          // Ahora que tenemos el idToken, realizamos la segunda solicitud
-          realizarSegundaSolicitud(idToken, monto, tipoDoc, documento, nombre);
+            // La primera solicitud se completó con éxito
+            const response = JSON.parse(xhrPrimeraSolicitud.responseText);
+            const idToken = response.data.IdToken;
+
+            // Ahora que tenemos el idToken, realizamos la segunda solicitud
+            realizarSegundaSolicitud(idToken, monto, tipoDoc, documento, nombre);
         } else {
-          // Manejar errores de la primera solicitud
-          console.error('Error en la primera solicitud:', xhrPrimeraSolicitud.status, xhrPrimeraSolicitud.statusText);
+            // Manejar errores de la primera solicitud
+            console.error('Error en la primera solicitud:', xhrPrimeraSolicitud.status, xhrPrimeraSolicitud.statusText);
         }
-      }
+        }
     };
-  
+
     // Enviar la primera solicitud con los datos de autenticación
     xhrPrimeraSolicitud.send(datosPrimeraSolicitudJSON);
-  }
-  
-  // Función para realizar la segunda solicitud con el idToken y datos del formulario
-  function realizarSegundaSolicitud(idToken, monto, tipoDoc, documento, nombre) {
+}
+
+// Función para realizar la segunda solicitud con el idToken y datos del formulario
+function realizarSegundaSolicitud(idToken, monto, tipoDoc, documento, nombre) {
+
+    const btn = document.getElementById('enviar');
+    const spinner = document.createElement('div');
+    spinner.className = 'sk-circle';
+
+    for (let i = 1; i <= 12; i++) {
+        const childDiv = document.createElement('div');
+        childDiv.className = `sk-circle${i} sk-child`;
+        spinner.appendChild(childDiv);
+    }
+
+    btn.disabled = true
+
+    btn.innerHTML = ''
+
+    btn.insertBefore(spinner, btn.firstChild)
+
     // URL del segundo servicio
     const segundoServicioURL = 'https://s5s6nqk77i.execute-api.us-east-1.amazonaws.com/generate_url';
-  
+
     // Datos para el cuerpo de la segunda solicitud
     const datosCuerpo = {
-      monto: monto,
-      referencia1: tipoDoc,
-      referencia2: documento,
-      referencia3: nombre
+        monto: monto,
+        referencia1: tipoDoc,
+        referencia2: documento,
+        referencia3: nombre
     };
-  
+
     // Convertir los datos del cuerpo a formato JSON
     const datosCuerpoJSON = JSON.stringify(datosCuerpo);
-  
+
     // Crear una nueva instancia de XMLHttpRequest para la segunda solicitud
     const xhrSegundaSolicitud = new XMLHttpRequest();
-  
+
     // Configurar la segunda solicitud POST
     xhrSegundaSolicitud.open('POST', segundoServicioURL, true);
-  
+
     // Configurar el encabezado de la segunda solicitud
     xhrSegundaSolicitud.setRequestHeader('Content-Type', 'application/json');
     xhrSegundaSolicitud.setRequestHeader('Authorization', idToken); // Agregar el token de autenticación
-  
+
     // Manejar la respuesta cuando se complete la segunda solicitud
     xhrSegundaSolicitud.onreadystatechange = function () {
-      if (xhrSegundaSolicitud.readyState === 4) {
+        if (xhrSegundaSolicitud.readyState === 4) {
         if (xhrSegundaSolicitud.status === 200) {
-          // La segunda solicitud se completó con éxito
-          const respuestaSegundaSolicitud = JSON.parse(xhrSegundaSolicitud.responseText);
-  
-          // Aquí puedes manejar la respuesta de la segunda solicitud
-          console.log('Respuesta de la segunda solicitud:', respuestaSegundaSolicitud);
-  
-          // Redireccionar al usuario a la URL obtenida, por ejemplo:
-          const redireccionURL = respuestaSegundaSolicitud.data.url;
-          window.location.href = redireccionURL;
+            // La segunda solicitud se completó con éxito
+            const respuestaSegundaSolicitud = JSON.parse(xhrSegundaSolicitud.responseText);
+            // Aquí puedes manejar la respuesta de la segunda solicitud
+            console.log('Respuesta de la segunda solicitud: Ok', respuestaSegundaSolicitud.statusCode + ' ' + respuestaSegundaSolicitud.message);
+            // Redireccionar al usuario a la URL obtenida, por ejemplo:
+            const redireccionURL = respuestaSegundaSolicitud.data.url;
+            // window.location.href = redireccionURL;
+            // window.open(redireccionURL, '_blank');
+
+            formulario.reset();
+            spinner.remove();
+            btn.innerHTML = 'Enviar solicitud';
+            btn.disabled = false;
+
+            const impurl = document.getElementById('authorizationUrl');
+            const urlimp = document.createElement('DIV');
+            urlimp.innerHTML = `
+            <div class="contenedorVentanaModal">
+                <div class="ventanaModal entrada">
+                <h2>¡Excelente!</h2>
+                <p class="textredirect">Serás direccionado a la pagina web de nuestro proveedor</p>
+                <div class="contenedorBtnsModal">
+                    <a href=${redireccionURL} target="_blanck" class="btnurl">Continuar</a>
+                    <button id="cerrarModal" class="btnurl">Cancelar</button>
+                </div>
+                </div>
+            </div>`;
+            const enviarsolicitud = urlimp.querySelector('.btnurl')
+            enviarsolicitud.addEventListener('click', () =>{
+                urlimp.remove();
+            })
+            const cerrarModalBtn = urlimp.querySelector('#cerrarModal');
+            cerrarModalBtn.addEventListener('click', () => {
+                urlimp.remove();
+            });
+            
+            impurl.appendChild(urlimp);
+
+
         } else {
-          // Manejar errores de la segunda solicitud
-          console.error('Error en la segunda solicitud:', xhrSegundaSolicitud.status, xhrSegundaSolicitud.statusText);
+            // Manejar errores de la segunda solicitud
+            console.error('Error en la segunda solicitud:', xhrSegundaSolicitud.status, xhrSegundaSolicitud.statusText);
         }
-      }
+        }
     };
-  
     // Enviar la segunda solicitud con el cuerpo de datos
     xhrSegundaSolicitud.send(datosCuerpoJSON);
-  }
+}
---
+++ jumseller.js
@@ -0,0 +1,132 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const formulario = document.querySelector('#transactionForm')
+    formulario.addEventListener('submit', (e) => {
+      e.preventDefault();
+      enviarFromulario();
+    });
+  });
+  
+  
+  function enviarFromulario(){
+  
+    const formulario = document.querySelector('#transactionForm')
+    const monto = parseInt(document.getElementById('monto').value);
+    const tipoDoc = document.getElementById('tipoDoc').value;
+    const documento = document.getElementById('documento').value;
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const btn = document.getElementById('enviar');
+    const responde = document.getElementById('authorizationUrl');
+  
+    console.log(monto)
+    //Spinner
+  
+    const spinner = document.createElement('div');
+    spinner.className = 'sk-circle';
+  
+    for (let i = 1; i <= 12; i++) {
+      const childDiv = document.createElement('div');
+      childDiv.className = `sk-circle${i} sk-child`;
+      spinner.appendChild(childDiv);
+    }
+  
+    btn.disabled = true
+  
+    btn.innerHTML = ''
+  
+    btn.insertBefore(spinner, btn.firstChild)
+  
+    //Spinner
+  
+    // FASE 2 - Generar el token de activación de la cuenta
+    const tokenEndpoint = 'https://s5s6nqk77i.execute-api.us-east-1.amazonaws.com/get_tokens_convenio_empresa';
+  
+  
+    // Realizar la solicitud POST para obtener el token de activación
+    fetch(tokenEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "username": "LUIS1234",
+        "password": "BDtn72@5"
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      // Obtener el token de activación [IdToken]
+  
+      const idToken = data.data.IdToken;
+  
+      // FASE 3 - Obtener la URL de autorización
+      const autorizacionEndpoint = 'https://s5s6nqk77i.execute-api.us-east-1.amazonaws.com/generate_url';
+  
+  
+      // Realizar la solicitud POST para obtener la URL de autorización
+      fetch(autorizacionEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': idToken // Agregar el token de activación en el encabezado de autorización
+        },
+        body: JSON.stringify({
+          "monto": monto,
+          "referencia1": tipoDoc,
+          "referencia2": documento,
+          "referencia3": nombre
+        })
+      })
+      .then(respuesta => respuesta.json())
+      .then(datos => {
+  
+        const redirec = datos.data.url;
+        console.log(redirec);
+  
+        formulario.reset();
+        spinner.remove()
+        btn.innerHTML = 'Solicitud enviada';
+        btn.disabled = false;
+        
+        // Redirecciona al usuario a la pasarela
+        //window.location.href = redirec;
+        //window.open(redirec, '_blank');
+        //window.open(encodeURIComponent(redirec), "_blank")
+        //responde.innerHTML = `<p>${datos.message}</p>`
+  
+        const impurl = document.getElementById('authorizationUrl');
+        const urlimp = document.createElement('DIV');
+        urlimp.innerHTML = `
+        <div class="contenedorVentanaModal">
+          <div class="ventanaModal entrada">
+            <h2>¡Excelente!</h2>
+            <p class="textredirect">Serás direccionado a la pagina web de nuestro proveedor</p>
+            <div class="contenedorBtnsModal">
+              <a href=${redirec} target="_blanck" class="btnurl">Continuar</a>
+              <button id="cerrarModal" class="btnurl">Cancelar</button>
+            </div>
+          </div>
+        </div>`;
+        const enviarsolicitud = urlimp.querySelector('.btnurl')
+        enviarsolicitud.addEventListener('click', () =>{
+          urlimp.remove();
+        })
+        const cerrarModalBtn = urlimp.querySelector('#cerrarModal');
+        cerrarModalBtn.addEventListener('click', () => {
+          urlimp.remove();
+        });
+        
+        impurl.appendChild(urlimp);
+  
+      })
+      .catch(error => {
+        console.error('Error al obtener la URL de autorización:', error);
+        
+      });
+    })
+    .catch(error => {
+      console.error('Error al generar el token de activación:', error);
+    });
+  
+  
+  }
